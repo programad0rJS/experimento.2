8a9ee04bb68acd8428651638b9c74c8f2d1a47f0
josue <josue@gmail.com>
2021-06-05 21:04:02 -0500

    subiendo

---
+++ precache-manifest.c01583c487221ed135b8bd6104e80406.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ecafd2bf22c84c0d9530525eadb0377",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f7a239b49a8eb5123e9",
+    "url": "/static/css/main.4c798faf.chunk.css"
+  },
+  {
+    "revision": "4eeb61d24d793d114467",
+    "url": "/static/js/2.e0ca3766.chunk.js"
+  },
+  {
+    "revision": "5f7a239b49a8eb5123e9",
+    "url": "/static/js/main.05edb9b1.chunk.js"
+  },
+  {
+    "revision": "4acd4b0b2e3598d28910",
+    "url": "/static/js/runtime-main.4a5b470e.js"
+  }
+]);
